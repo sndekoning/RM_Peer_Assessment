f872cee7185f7e8e519a5e7448ed93f272631a78
sndekoning <sylvester.de.koning90@gmail.com>
2016-03-23 15:52:38 +0100

    Added scripts and start of markdown file

---
+++ mt.R
@@ -0,0 +1,27 @@
+# Loading in the data.
+data("mtcars")
+
+# Transforming the proper variables to factor variables. 
+cols <- c("cyl", "vs", "gear", "carb")
+mtcars[, cols] <- lapply(mtcars[, cols], as.factor)
+mtcars$am <- factor(mtcars$am, levels = c("0", "1"), labels = c("Automatic", "Manual"))
+
+# Some exploratory analysis.
+summary(mtcars)
+boxplot(mpg ~ am, data = mtcars)
+
+pairs(mtcars, panel = function(x, y){
+    points(x, y)
+    abline(lm(y~x), col = "red")
+})
+
+# Testing for significant differences between MPG for TT
+shapiro.test(mtcars$mpg) # Assumption of normalcy holds
+t.test(mpg ~ am, data = mtcars)
+
+# Fitting a linear regression model.
+fit <- lm(mpg ~ am, data = mtcars)
+summary(fit)
+
+fitall <- lm(mpg ~ ., data = mtcars)
+summary(fitall)
